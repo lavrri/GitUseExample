91d1ad043ed75d98a91aaf1ef7716033d978e723
Ivan Lavrov <ivanlavrov@Ivans-MacBook-Air.local>
2018-07-03 23:43:33 +0300

    Map added

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Ivan Lavrov on 7/3/18.
+//  Copyright © 2018 Ivan Lavrov. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary: NSObject {
+    
+}
--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
 
 
